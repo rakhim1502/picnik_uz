ed58056222af8db9be9f61ffcb3fec3318619036
rakhim1502 <uzbrm95@gmail.com>
2026-06-14 18:24:26 +0500

    fix cors settings

--- server/index.js
+++ server/index.js
@@ -13,7 +13,13 @@ dotenv.config();
 const app = express();
 
 // Middleware - LIMIT QO'SHILDI!
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://localhost:5173', // Local development
+        'https://picnik-uz.vercel.app/'
+    ],
+    credentials: true
+}));
 app.use(express.json({ limit: '10mb' }));  // ← 10MB gacha
 app.use(express.urlencoded({ limit: '10mb', extended: true }));  // ← 10MB gacha
 
